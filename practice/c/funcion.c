33e0b28e35a8c2e5ea2daff9a45f86e5dde33764
chaniyacom <chaniyacom@gmail.com>
2026-05-27 12:48:10 +0000

    first commit

--- practice/c/funcion.c
+++ practice/c/funcion.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int a=1;
-    printf("%d", a);
+    int age = 20;
+    double height = 175.5;
+    char grade = 'A';
+
+    printf("나이: %d살\n", age);
+    printf("키: %fcm\n", height);
+    printf("학점: %c\n", grade);
+
+    printf("키: %.1fcm\n", height);
     return 0;
 }
